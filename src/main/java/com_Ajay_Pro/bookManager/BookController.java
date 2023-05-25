679f35647610c60361357ed5bd7d2e249d52056c
Ajaykumaryd <ajaykumaryd2001@gmail.com>
2023-05-25 16:53:51 +0530

    new APIs Added

--- src/main/java/com_Ajay_Pro/bookManager/BookController.java
+++ src/main/java/com_Ajay_Pro/bookManager/BookController.java
@@ -1,19 +1,34 @@
 package com_Ajay_Pro.bookManager;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController //it helps springboot that all apis are inside the particular class where rest is being written
                 // shows are APIs are
 public class BookController {
-    Map <Integer,Book> bookData=new HashMap<>();
+    Map <Integer,Book> bookData=new HashMap<>();  //Integer are keys because they are unique
     //book created
     @PostMapping("/add-new-book")  //api call        //Postmapping above the method it is using
     public String addBook(@RequestBody Book book){    //request body before input variable
-     return "book added with id"+book.getId();
+        bookData.put(book.getId(),book);
+        return "book added with id "+book.getId();
     }
+
+
+    @GetMapping("/get-book") //google.com/search?q=yes_bank   localhost:8080//get-book?id=1
+    public Book getBook(@RequestParam Integer id){    //For String= @RequestParam String q
+        return bookData.get(id);
+    }
+
+    @GetMapping("/get-list-books")
+    public List<Book> getBooks(){
+        return new ArrayList<>(bookData.values());
+    }
+
+
 }
+
--- src/main/java/com_Ajay_Pro/bookManager/BookManagerApplication.java
+++ src/main/java/com_Ajay_Pro/bookManager/BookManagerApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookManagerApplication {
+ public class BookManagerApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(BookManagerApplication.class, args);
 
-		 Book a=new Book(25,"the man",100,"ajay");
+//		 Book a=new Book(25,"the man",100,"ajay");
 
 	}
 
