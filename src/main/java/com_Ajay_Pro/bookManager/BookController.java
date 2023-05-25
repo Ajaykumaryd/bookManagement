5593112e09cce408a7dc45c6361c21ccd29f1ff4
Ajaykumaryd <ajaykumaryd2001@gmail.com>
2023-05-25 12:10:10 +0530

    restController added

--- src/main/java/com_Ajay_Pro/bookManager/BookController.java
+++ src/main/java/com_Ajay_Pro/bookManager/BookController.java
@@ -1,15 +1,19 @@
 package com_Ajay_Pro.bookManager;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.HashMap;
 import java.util.Map;
+
+@RestController //it helps springboot that all apis are inside the particular class where rest is being written
+                // shows are APIs are
 public class BookController {
-
     Map <Integer,Book> bookData=new HashMap<>();
-
-
     //book created
-    public String addBook(Book book){
-     bookData.put(book.getId(), book) ;
-     return "book added";
+    @PostMapping("/add-new-book")  //api call        //Postmapping above the method it is using
+    public String addBook(@RequestBody Book book){    //request body before input variable
+     return "book added with id"+book.getId();
     }
 }
